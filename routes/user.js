acb1c3998762d3868413d4c7047641b5ff7068aa
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2025-01-24 18:32:48 +0530

    feat: user login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,11 @@ const express = require("express");
 const User = require("../models/user.models");
 const bcrypt = require("bcrypt");
 const router = express.Router();
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+dotenv.config();
+const secretKey = process.env.JWT_Secret;
+
 
 router.post("/register", async (req, res) => {
   const { username, email, mobile, password, confirmPassword } = req.body;
@@ -30,6 +35,34 @@ router.post("/register", async (req, res) => {
       .json({ message: error.message });
   }
 });
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Either Email is wrong or Password is wrong" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Either Email is wrong or Password is wrong" });
+    }
+    try {
+      const payload = {
+        id: user._id,
+      };
+      const token = await jwt.sign(payload, secretKey);
+      return res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "User cannot login", error: error.message });
+    }
+  });
+  
 
 
 
