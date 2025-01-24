767fe0529df0fcc8f5eeea340ab9d319e08efa7a
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2025-01-24 18:45:58 +0530

    feat: user details by id

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,18 @@ const jwt = require("jsonwebtoken");
 dotenv.config();
 const secretKey = process.env.JWT_Secret;
 
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
 
 router.post("/register", async (req, res) => {
   const { username, email, mobile, password, confirmPassword } = req.body;
@@ -30,40 +42,35 @@ router.post("/register", async (req, res) => {
     });
     return res.status(200).json({ message: "User registered successfully" });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 });
 
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res
-        .status(400)
-        .json({ message: "Either Email is wrong or Password is wrong" });
-    }
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res
-        .status(400)
-        .json({ message: "Either Email is wrong or Password is wrong" });
-    }
-    try {
-      const payload = {
-        id: user._id,
-      };
-      const token = await jwt.sign(payload, secretKey);
-      return res.status(200).json({ message: "Login successful", token });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ message: "User cannot login", error: error.message });
-    }
-  });
-  
-
-
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "Either Email is wrong or Password is wrong" });
+  }
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res
+      .status(400)
+      .json({ message: "Either Email is wrong or Password is wrong" });
+  }
+  try {
+    const payload = {
+      id: user._id,
+    };
+    const token = await jwt.sign(payload, secretKey);
+    return res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "User cannot login", error: error.message });
+  }
+});
 
 module.exports = router;
