b1fa58e81d4d2fde41e46594a76bfb110e17f7f5
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2025-01-24 17:27:22 +0530

    feat: database connected

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/data");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+
+const app = express();
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server runnning on port ${PORT}`);
+});
